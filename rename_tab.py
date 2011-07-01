56bf89162aca69b4f1b7808a1f1cde6dc5f3befc
Jarod Reid <jarod.reid@moneydesktop.com>
2011-07-01 12:56:38 -0600

    support for better renaming

--- rename_tab.py
+++ rename_tab.py
@@ -2,7 +2,7 @@ import sublime, sublime_plugin, os, shutil
 
 class RenameTabCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    self.view.window().show_input_panel("Rename Tab:", "", lambda s: self.rename(s), None, None)
+    self.view.window().show_input_panel("Rename File:", "", lambda s: self.rename(s), None, None)
   
   def rename(self, new_file):
     old_file = self.view.file_name()
@@ -17,12 +17,15 @@ class RenameTabCommand(sublime_plugin.TextCommand):
       return
     if(new_file == old_file):
       return
+    temp_file = file_dir + os.sep + "_" + old_file_name
     new_file = file_dir + os.sep + new_file
-    shutil.copyfile(old_file, new_file)
-    if os.path.isfile(new_file):
-      self.view.window().run_command("open", new_file)
-      self.view.window().run_command("close")
+    shutil.copyfile(old_file, temp_file)
+    if os.path.isfile(temp_file):
+      window = self.view.window()
+      window.run_command("close")
+      os.remove(old_file)
+      shutil.move(temp_file, new_file)
+      window.open_file(new_file)
       if old_file.endswith(".py"):
         os.remove(old_file + "c")
-      os.remove(old_file)
-     
+      
